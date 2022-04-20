7072cd0ef82270778de3e8d7195ef81c46e7c72a
Anusiya-devi-S-D <103924975+Anusiya-devi-S-D@users.noreply.github.com>
2022-04-20 18:40:46 +0530

    Add ./integrationcenter/src/main/webapp/app.js/url_config.js

--- integrationcenter/src/main/webapp/app.js/url_config.js
+++ integrationcenter/src/main/webapp/app.js/url_config.js
@@ -1,20 +1,21 @@
- "use strict";
-  var app = angular.module("urlConfig", ["ngRoute", "ngGrid"]);
-  app.service('CONFIG', function($location) {
-	        console.log("config.js :: app.service :: Refering urls")
-	        var Url = {
-		              'projectAnalysis_url': 'http://${local.load_blncr_ip}:8007',
-		              'dashboard_url': '',
-		              'codeReview_url': 'http://${local.load_blncr_ip}:8001',
-		              'Deployment_url': 'http://${local.load_blncr_ip}:8010',
-		              'onboarding_url': 'http://${local.load_blncr_ip}:8008',
-		              'estimation_url': 'http://${local.load_blncr_ip}:8003',
-		              'apiSpecs_url': 'http://${local.load_blncr_ip}:8004',
-		              'integrationcenter_url': 'http://${local.load_blncr_ip}:8080',
-		              'specAnalyzer_url': 'http://${local.load_blncr_ip}:8006',
-		              'inception_url': 'http://${local.load_blncr_ip}:8989',
-		              'logout_url': '',
-		              'migrationcenter_url': 'http://${local.load_blncr_ip}:8877'
-	          };
-	          return Url;
-  });
+"use strict";
+var app = angular.module("urlConfig", ["ngRoute", "ngGrid"]);
+app.service('CONFIG', function($location) {
+       console.log("config.js :: app.service :: Refering urls")
+       var Url = {
+              'projectAnalysis_url': 'http://34.72.183.6:8007',
+              'dashboard_url': '',
+              'codeReview_url': 'http://34.72.183.6:8001',
+              'Deployment_url': 'http://34.72.183.6:8010',
+              'onboarding_url': 'http://34.72.183.6:8008',
+              'estimation_url': 'http://34.72.183.6:8003',
+              'apiSpecs_url': 'http://34.72.183.6:8004',
+              'integrationcenter_url': 'http://34.72.183.6:8080',
+              'specAnalyzer_url': 'http://34.72.183.6:8006',
+              'inception_url': 'http://34.72.183.6:8989',
+              'logout_url': '',
+              'migrationcenter_url': 'http://34.72.183.6:8877'
+         };
+         return Url;
+});
+
