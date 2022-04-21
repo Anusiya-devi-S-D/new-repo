9cb13af9b551af98e99ae0b2e6cb7a1b8318aea7
Anusiya-devi-S-D <103924975+Anusiya-devi-S-D@users.noreply.github.com>
2022-04-21 17:39:10 +0530

    Added load_balancer_ip

--- integrationcenter/src/app/mainapp.js/url_config.js
+++ integrationcenter/src/app/mainapp.js/url_config.js
@@ -3,18 +3,18 @@ var app = angular.module("urlConfig", ["ngRoute", "ngGrid"]);
 app.service('CONFIG', function($location) {
        console.log("config.js :: app.service :: Refering urls")
        var Url = {
-              'projectAnalysis_url': 'http://35.232.126.56:8007',
+              'projectAnalysis_url': 'http://104.154.177.22:8007',
               'dashboard_url': '',
-              'codeReview_url': 'http://35.232.126.56:8001',
-              'Deployment_url': 'http://35.232.126.56:8010',
-              'onboarding_url': 'http://35.232.126.56:8008',
-              'estimation_url': 'http://35.232.126.56:8003',
-              'apiSpecs_url': 'http://35.232.126.56:8004',
-              'integrationcenter_url': 'http://35.232.126.56:8080',
-              'specAnalyzer_url': 'http://35.232.126.56:8006',
-              'inception_url': 'http://35.232.126.56:8989',
+              'codeReview_url': 'http://104.154.177.22:8001',
+              'Deployment_url': 'http://104.154.177.22:8010',
+              'onboarding_url': 'http://104.154.177.22:8008',
+              'estimation_url': 'http://104.154.177.22:8003',
+              'apiSpecs_url': 'http://104.154.177.22:8004',
+              'integrationcenter_url': 'http://104.154.177.22:8080',
+              'specAnalyzer_url': 'http://104.154.177.22:8006',
+              'inception_url': 'http://104.154.177.22:8989',
               'logout_url': '',
-              'migrationcenter_url': 'http://35.232.126.56:8877'
+              'migrationcenter_url': 'http://104.154.177.22:8877'
          };
          return Url;
 });
